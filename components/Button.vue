228806a555a6c33dd483659820b46579ce9e2a64
susiegithub <sukyyuha@hotmail.com>
2025-02-01 17:39:52 +0000

    buttons with icons working

---
+++ components/Button.vue
@@ -0,0 +1,54 @@
+<template>
+    <button :class="buttonClass" @click="handleClick">
+        <FontAwesomeIcon v-if="icon" :icon="icon" class="button-icon" />
+
+        <slot />
+    </button>
+</template>
+  
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+name: 'Button',
+    props: {
+        type: {
+            type: String,
+            default: 'button'
+        },
+        buttonClass: {
+            type: String,
+            default: ''
+        },
+        icon: Object 
+    },
+    methods: {
+        handleClick(event) {
+            this.$emit('click', event);
+        }
+    }
+};
+
+</script>
+
+<style scoped>
+.icon-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+  background: url('/icon.svg') no-repeat center center;
+  background-size: contain;
+  border: none;
+  cursor: pointer;
+}
+
+.header-bar {
+  background-color: #b3d9ea; /* Light blue */
+  padding: 15px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+      <div class="header-bar">
+        <h1>Pathology heatmap generator</h1>
+      </div>
+      
+      <div v-for = "button in buttons" :key="button.label">
+        <Button :buttonClass="button.class" @click="button.action"> <FontAwesomeIcon :icon="button.icon" class="fa-icon" />
+          {{ button.label }}
+        </Button>
+      </div>
+    </div>
+</template>
+
+
+<script>
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faSearchPlus, faSearchMinus, faUndo, faRedo, faExpand, faPowerOff, faInfo } from '@fortawesome/free-solid-svg-icons'
+
+/* Register the icons into the library */
+library.add(faSearchPlus, faSearchMinus, faUndo, faRedo, faExpand, faPowerOff, faInfo)
+
+
+export default {
+    components: {
+      FontAwesomeIcon
+    },
+
+    data() {
+        return {
+            buttons: [
+            { label: "Zoom In", class: "secondary", action: this.zoomIn, icon: "search-plus" },
+            { label: "Zoom Out", class: "secondary", action: this.zoomOut, icon: "search-minus" },
+            { label: "Rotate Left", class: "secondary", action: this.rotateLeft, icon: "undo" },
+            { label: "Rotate Right", class: "secondary", action: this.rotateRight, icon: "redo"},
+            { label: "Full Screen", class: "secondary", action: this.fullScreen, icon: "expand" },
+            { label: "Toggle heatmap", class: "success", action: this.toggleHeatmap, icon: "power-off" },
+            { label: "Info", class: "secondary", action: this.showInfo, icon: "info" },
+            ]
+        };
+    },
+
+    methods: {
+        zoomIn() {
+        alert("Zooming In!");
+        },
+        zoomOut() {
+        alert("Zooming Out!");
+        },
+        rotateLeft() {
+        alert("Rotating Left!");
+        },
+        rotateRight() {
+        alert("Rotating Right!");
+        },
+        fullScreen() {
+        alert("Full Screen!");
+        },
+        toggleHeatmap() {
+        alert("Toggle Heatmap!");
+        },
+        showInfo() {
+        alert("Show info!");
+        }
+    }
+};
+</script>
